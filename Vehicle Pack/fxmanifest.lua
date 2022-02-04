090341e095dd6c75e5e2fdc0a6ee1eba951bf0a3
AvaN0x <clement.ricatte57@gmail.com>
2022-02-04 16:39:56 +0100

    fix(fxmanifest): :wrench: Use globbing in manifest
    https://docs.fivem.net/docs/scripting-reference/resource-manifest/resource-manifest/#globbing

--- Vehicle Pack/fxmanifest.lua
+++ Vehicle Pack/fxmanifest.lua
@@ -1,99 +1,18 @@
-fx_version 'bodacious'
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
 
- 
- --CAR NAME HERE
 files {
-    'data/CAR NAME HERE/vehicles.meta',
-    'data/CAR NAME HERE/carvariations.meta',
-    'data/CAR NAME HERE/carcols.meta',
-    'data/CAR NAME HERE/handling.meta',
-    'data/CAR NAME HERE/vehiclelayouts.meta',
+    "data/**/vehicles.meta",
+    "data/**/carvariations.meta",
+    "data/**/carcols.meta",
+    "data/**/handling.meta",
+    "data/**/vehiclelayouts.meta",
 }
 
-data_file 'HANDLING_FILE'            'data/CAR NAME HERE/handling.meta'
-data_file 'VEHICLE_METADATA_FILE'    'data/CAR NAME HERE/vehicles.meta'
-data_file 'CARCOLS_FILE'             'data/CAR NAME HERE/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE'   'data/CAR NAME HERE/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'     'data/CAR NAME HERE/vehiclelayouts.meta'
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
 
-
-
-
- --CAR NAME HERE
-files {
-    'data/CAR NAME HERE/vehicles.meta',
-    'data/CAR NAME HERE/carvariations.meta',
-    'data/CAR NAME HERE/carcols.meta',
-    'data/CAR NAME HERE/handling.meta',
-    'data/CAR NAME HERE/vehiclelayouts.meta',
-}
-
-data_file 'HANDLING_FILE'            'data/CAR NAME HERE/handling.meta'
-data_file 'VEHICLE_METADATA_FILE'    'data/CAR NAME HERE/vehicles.meta'
-data_file 'CARCOLS_FILE'             'data/CAR NAME HERE/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE'   'data/CAR NAME HERE/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'     'data/CAR NAME HERE/vehiclelayouts.meta'
-
-
-
-
- --CAR NAME HERE
-files {
-    'data/CAR NAME HERE/vehicles.meta',
-    'data/CAR NAME HERE/carvariations.meta',
-    'data/CAR NAME HERE/carcols.meta',
-    'data/CAR NAME HERE/handling.meta',
-    'data/CAR NAME HERE/vehiclelayouts.meta',
-}
-
-data_file 'HANDLING_FILE'            'data/CAR NAME HERE/handling.meta'
-data_file 'VEHICLE_METADATA_FILE'    'data/CAR NAME HERE/vehicles.meta'
-data_file 'CARCOLS_FILE'             'data/CAR NAME HERE/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE'   'data/CAR NAME HERE/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'     'data/CAR NAME HERE/vehiclelayouts.meta'
-
-
-
-
- --CAR NAME HERE
-files {
-    'data/CAR NAME HERE/vehicles.meta',
-    'data/CAR NAME HERE/carvariations.meta',
-    'data/CAR NAME HERE/carcols.meta',
-    'data/CAR NAME HERE/handling.meta',
-    'data/CAR NAME HERE/vehiclelayouts.meta',
-}
-
-data_file 'HANDLING_FILE'            'data/CAR NAME HERE/handling.meta'
-data_file 'VEHICLE_METADATA_FILE'    'data/CAR NAME HERE/vehicles.meta'
-data_file 'CARCOLS_FILE'             'data/CAR NAME HERE/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE'   'data/CAR NAME HERE/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'     'data/CAR NAME HERE/vehiclelayouts.meta'
-
-
-
- --CAR NAME HERE
-files {
-    'data/CAR NAME HERE/vehicles.meta',
-    'data/CAR NAME HERE/carvariations.meta',
-    'data/CAR NAME HERE/carcols.meta',
-    'data/CAR NAME HERE/handling.meta',
-    'data/CAR NAME HERE/vehiclelayouts.meta',
-}
-
-data_file 'HANDLING_FILE'            'data/CAR NAME HERE/handling.meta'
-data_file 'VEHICLE_METADATA_FILE'    'data/CAR NAME HERE/vehicles.meta'
-data_file 'CARCOLS_FILE'             'data/CAR NAME HERE/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE'   'data/CAR NAME HERE/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE'     'data/CAR NAME HERE/vehiclelayouts.meta'
-
-
-
-
-
-
-
-client_script {
-    'veh_names.lua'
-}
+client_script {"veh_names.lua"}
